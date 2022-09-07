4a582fb3316c87717d7f4a16d21adff45aa64b36
Stephen Bacso <stephen.bacso@gmail.com>
2022-09-07 11:41:54 -0400

    added cards for event list

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -12,7 +12,7 @@ function MainNavigation() {
             <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">New Meetup</Link>
+            <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
             <Link to="/favorites">My Favorites</Link>
--- src/components/layout/meetups/MeetupList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MeetupList = (props) => {
-  return <div></div>;
-};
-
-export default MeetupList;
